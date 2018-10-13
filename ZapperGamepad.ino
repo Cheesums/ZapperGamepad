bfe849d4fd57716a2098cf7d741ae7f12de5b5e3
Cheesums <swiftcheese013@gmail.com>
2018-10-12 20:31:19 -0400

    Add arrow drawing

--- ZapperGamepad.ino
+++ ZapperGamepad.ino
@@ -43,13 +43,13 @@ class Circle:public Button
   
   void draw(bool isInv)
   {
-    int buttonPosX = (30*posX)+64;
-    int buttonPosY = (30*posY)+49;
+    int buttonPosX = (29*posX)+64;
+    int buttonPosY = (29*posY)+48;
 
     int circleFill = 2-(3*isInv);
 
     TV.println(buttonPosX-2, buttonPosY-3, faceChar);
-    TV.draw_circle(buttonPosX, buttonPosY, 7, 1, circleFill);
+    TV.draw_circle(buttonPosX, buttonPosY, 10, 1, circleFill);
   };
 };
 
@@ -77,27 +77,56 @@ class Arrow:public Button
 {
   public:
 
-  int posX;
-  int posY;
-  int rotation;
+  int posX; //-1 for left, 1 for right, 0 for middle
+  int posY; //-1 for above, 1 for below, 0 for middle
 
-  Arrow()
-  {
-    
-  }
-
-  Arrow(int newPosX, int newPosY, int newRotation, bool newIsSet):Button(newIsSet)
+  Arrow(int newPosX, int newPosY, bool newIsSet): Button(newIsSet)
   {
     posX = newPosX;
     posY = newPosY;
-    rotation = newRotation;
+  }
+
+  Arrow()
+  {
+
   }
 
   void draw(bool isInv)
   {
+    
+    int rectX = 58+(20*posX);
+    int rectY = 42+(20*posY);
+    int deltX = 12+abs(posX);
+    int deltY = 12+abs(posY);
 
-  }
+    TV.draw_rect(rectX, rectY, deltX, deltY, 1, !isInv);
 
+    //if is Inv
+      //stuff
+    //else
+      
+      int arrOffX0 = (-7*abs(posY))+((((1+posX)/2)*11)+((-1+posX)/2))*posX;
+      int arrOffY0 = ((((1+posY)/2)*11)+((-1+posY)/2))*posY+(-7*abs(posX));
+      int arrOffX1 = (20*abs(posY))+((((1+posX)/2)*11)+((-1+posX)/2))*posX;
+      int arrOffY1 = ((((1+posY)/2)*11)+((-1+posY)/2))*posY+(19*abs(posX));
+
+      int arrMultX0 = posX+1-abs(posX);
+      int arrMultY0 = posY+1-abs(posY);
+      int arrMultX1 = (-1*abs(posY))+posX;
+      int arrMultY1 = posY+(-1*abs(posX));
+
+      int arrPosX0 = rectX+arrOffX0;
+      int arrPosY0 = rectY+arrOffY0;
+      int arrPosX1 = rectX+arrOffX1;
+      int arrPosY1 = rectY+arrOffY1;
+
+      
+
+      for(int i = 0; i < 14; i++)
+      {
+        TV.draw_line(arrPosX0+(i*arrMultX0), arrPosY0+(i*arrMultY0), arrPosX1+(i*arrMultX1), arrPosY1+(i*arrMultY1), 1);
+      }
+  };
 };
 
 int Button = 4;
@@ -115,12 +144,22 @@ void setup()
   Circle buttonC('C',-1, 1, 0);
   Circle buttonD('D', 1, 1, 0);
 
+  Arrow upArrow(0, -1, false);
+  Arrow downArrow(0, 1, false);
+  Arrow leftArrow(-1, 0, false);
+  Arrow rightArrow(1, 0, false);
+
   Rectangle buttonMid(0);
   
   buttonA.draw(false);
   buttonB.draw(false);
   buttonC.draw(false);
   buttonD.draw(false);
+
+  upArrow.draw(false);
+  downArrow.draw(false);
+  leftArrow.draw(false);
+  rightArrow.draw(false);
 
   buttonMid.draw(false);
 
